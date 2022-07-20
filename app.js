59aece4cdf4be61d34ed947a20a355b0f903f0dc
Dibrary <kkdh828@hanmail.net>
2022-07-20 14:14:47 +0900

    220720 D3 transition, on, delay, duration

--- app.js
+++ app.js
@@ -5,14 +5,20 @@ const path = require('path');
 const app = express();
 app.set('port', process.env.PORT || 3000);
 
+app.use('/', express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, '/first.html'));
 })
 
-app.get('/verticalBar', (req,res) => {
-    res.sendFile(path.join(__dirname, '/가로막대그래프.html'));
+app.get('/horizontalBar', (req,res) => {
+    res.sendFile(path.join(__dirname, '/charts/가로막대그래프.html'));
 });
 
+
+app.get('/verticalBar', (req,res) => {
+    res.sendFile(path.join(__dirname, '/charts/세로막대그래프.html'));
+});
 
 app.listen(app.get('port'), () => {
     console.log(app.get('port'), '번 포트에서 대기 중');
